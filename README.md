6e9b1bfbd2a621339e7a3a7b8b7063b2b6584f76
Seb Julliand <sebjulliand@gmail.com>
2024-01-29 19:31:06 +0100

    Initialized branch for Code for IBM i based devs
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VSCode Template extension
-This is my template to create a VScode extension
+This is my template to create a VScode extension that uses [Code for IBM i]([https://](https://github.com/codefori/vscode-ibmi))
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,24 @@
+import { CodeForIBMi } from '@halcyontech/vscode-ibmi-types';
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log("Template activated");
-	
+export let code4i: CodeForIBMi;
+
+export async function activate(context: vscode.ExtensionContext) {
+	const codeForIBMiExtension = vscode.extensions.getExtension<CodeForIBMi>('halcyontechltd.code-for-ibmi');
+	if (codeForIBMiExtension) {
+		code4i = codeForIBMiExtension.isActive ? codeForIBMiExtension.exports : await codeForIBMiExtension.activate();
+	}
+	else {
+		throw new Error(vscode.l10n.t("This extension requires the 'halcyontechltd.code-for-ibmi' extension to be activated!"));
+	}
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand('template.helloWorld', () => vscode.window.showInformationMessage(vscode.l10n.t('Hello')))
 	);
+
+	console.log("Extension activated");
 }
 
-export function deactivate() { 
-	console.log("Template deactivated");
+export function deactivate() {
+	console.log("Extension deactivated");
 }
